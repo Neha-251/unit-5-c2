892402f182ddbedf69be677e6454d885a42caccf
Neha Sen <neha712513@gmail.com>
2022-04-18 14:51:58 +0530

    ok

--- unit-5-c2-eval/src/components/AddStudent.jsx
+++ unit-5-c2-eval/src/components/AddStudent.jsx
@@ -1,10 +1,35 @@
-import React from "react";
+import {useState} from "react";
+import axios from "axios";
+
+function AddStudent({toggleDisplay}) {
+
+    const [formData, setFormData] = useState({
+        first_name: "",
+        last_name: "",
+        email: "",
+        gender: "",
+        age: "",
+        tenth_score: "",
+        twelth_score: "",
+        preferred_branch: ""
+    })
+
+    function handleChange(e) {
+        //let {cla}
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault;
+
+        axios.post("http://localhost:8080/students", formData).then(toggleDisplay());
+    }
 
 
-function AddStudent() {
 
 
-    return 
+    return (
+
+    )
 }
 
 
