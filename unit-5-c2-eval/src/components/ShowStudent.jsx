a62ace72bfee34f03b2be134326da4c607a6c571
Neha Sen <neha712513@gmail.com>
2022-04-18 16:11:12 +0530

    ok

--- unit-5-c2-eval/src/components/ShowStudent.jsx
+++ unit-5-c2-eval/src/components/ShowStudent.jsx
@@ -1,18 +1,49 @@
-//import {useState, useEffect} from "react";
+import {useState, useEffect} from "react";
 
 
 function ShowStudent({data, setData}){
 
-    //const [text, setText] = useState("");
+    const [text, setText] = useState("");
 
     return (
         <div className="container">
-            <div className="subCon">
-                <button onClick={()=> {
-                    //let newarr = [...data]
+            <table border="1">
 
-                }}>click</button>
-            </div>
+                <thead>
+                    <tr>
+                        <th>Sl No.</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                        <th>Gender</th>
+                        <th>Age</th>
+                        <th>Thenth Score</th>
+                        <th>Twlfth Score</th>
+                        <th>Preferred Batch</th>
+                    </tr>
+                </thead>
+                <tbody>
+
+                {data.map((student, index) => {
+                    return (
+                        <tr key={student.id} className="studentDetails">
+
+                            <td>{student.id}</td>
+                            <td className="">{student.first_name}</td>
+                            <td>{student.last_name}</td>
+                            <td>{student.email}</td>
+                            <td>{student.gender}</td>
+                            <td>{student.age}</td>
+                            <td>{student.tenth_score}</td>
+                            <td>{student.twelth_score}</td>
+                            <td>{student.preferred_branch}</td>
+
+
+                        </tr>
+                    )
+                })}
+                </tbody>
+            </table>
         </div>
     )
 }
